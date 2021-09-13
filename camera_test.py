887e157a9f141ac2524d085ad195aa9843881760
MikkelHebel <88710599+MikkelHebel@users.noreply.github.com>
2021-09-13 04:50:59 -0400

    Add files via upload

---
+++ camera_test.py
@@ -0,0 +1,41 @@
+import numpy as np
+import cv2
+import os
+import time
+
+cascPath=os.path.dirname(cv2.__file__)+"/data/haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+print("[INFO] Detector set!")
+
+last_time = time.time()
+print("[INFO] Time set!")
+print("[INFO] Setting Video Capture device...")
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+print("[INFO] Video Capture device set!")
+cap.set(3,640) # set Width
+cap.set(4,480) # set Height
+while(True):
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.3,
+        minNeighbors=5,
+        minSize=(30, 30),
+        #flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+
+    cv2.imshow('frame', frame)
+    print('Frame time: {} seconds'.format(time.time()-last_time))
+
+    k = cv2.waitKey(30) & 0xff
+    if k == 27: # press 'ESC' to quit
+        break
+cap.release()
+cv2.destroyAllWindows()
